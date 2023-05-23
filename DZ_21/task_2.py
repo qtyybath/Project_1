a2a11da6fdfa00a31b8b56f9037c5f26f899a5e0
qtyybath <buhfxy@gmail.com>
2023-05-23 23:53:48 +0200

    DZ_21

---
+++ DZ_21/task_2.py
@@ -0,0 +1,9 @@
+# 2. Створити SQL запити для додавання записів у створену таблицю.
+# Створити запити для додавання мінімум 5 різних записів.
+# Формат здачі:
+# Запити зберегти в окремий файл і запушити на git-репозиторій
+
+
+# INSERT INTO table_data(first_name, last_name, age)
+# VALUES ('Megan', 'Fox', 37), ('Katy', 'Perry', 38), ('Tom', 'Hiddleston', 42),
+#        ('Leonardo', 'DiCaprio', 48 ), ('Jennifer', 'Aniston', 54);
